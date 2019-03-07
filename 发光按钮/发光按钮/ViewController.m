9db13754521de0dfee0be6677412e9b62f1cef45
Snake <379957102@qq.com>
2019-03-07 10:56:25 +0800

    update

--- 发光按钮/发光按钮/ViewController.m
+++ 发光按钮/发光按钮/ViewController.m
@@ -10,14 +10,28 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UIButton* button;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor blackColor];
+    //
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.view addSubview:button];
+    button.frame = CGRectMake(100, 100, 50, 50);
+    [button setTitle:@"click" forState:UIControlStateNormal];
+    button.titleLabel.font = [UIFont systemFontOfSize:20];
+    [button addTarget:self action:@selector(buttonClick:) forControlEvents:UIControlEventTouchUpInside];
+    
 }
 
+- (void)buttonClick:(UIButton *)btn {
+//    btn.showsTouchWhenHighlighted = YES;
+    [btn setShowsTouchWhenHighlighted:YES];
+}
 
 @end
